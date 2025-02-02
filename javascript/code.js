2e272a988e6df91ed96ad1892db1044cca9569eb
kanishkachandran <kanishka.kcn@gmail.com>
2025-02-02 11:44:47 +0530

    new update

---
+++ javascript/code.js
@@ -0,0 +1,36 @@
+// console
+console.log("kanishka")
+console.log(15)
+console.log(15.88)
+console.log(true)
+console.log([12,13,14,15,53])
+console.table({'name':"kanishka",'id':7893})
+console.error("sample error")
+console.warn("warning msg")
+
+
+// varibale
+
+// var
+// let
+// const
+
+var b;
+b=30;
+
+var b=30;
+var c=40;
+console.log(b+c)
+
+let f=30;
+let h=40;
+console.log(f+h)
+
+if(true){
+    let num="vaild number"
+    console.log(num)
+}
+console.log(num)
+
+const p=90;
+// const p=100;
